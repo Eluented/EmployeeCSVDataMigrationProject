7713aa9fde9bc30b97202ee1799eef3efa9ed408
Omar Hussain <ohussain1507@gmail.com>
2023-04-12 23:19:44 +0100

    test cases for phase 2

---
+++ src/test/java/com/sparta/room3/ConnectionProviderTest.java
@@ -0,0 +1,67 @@
+package com.sparta.room3;
+
+import com.sparta.room3.model.ConnectionProvider;
+import org.junit.jupiter.api.*;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConnectionProviderTest {
+
+    @BeforeAll
+    public static void setup() {
+        // Create a new database and populate it with some test data
+        // This is only needed if there is no existing database with the same name and structure
+        // Alternatively, you can use a separate test database for your tests
+        try (Connection connection = ConnectionProvider.getConnection()) {
+            TestDataGenerator testDataGenerator = new TestDataGenerator();
+            testDataGenerator.createTestDatabase(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            fail("Failed to set up test database");
+        }
+    }
+
+    @AfterAll
+    public static void cleanup() {
+        // Delete the test data from the database and close the connection
+        try (Connection connection = ConnectionProvider.getConnection()) {
+            TestDataGenerator.deleteTestDatabase(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            fail("Failed to clean up test database");
+        }
+    }
+
+    @Test
+    public void testGetConnection() {
+        // Verify that getConnection returns a non-null connection object
+        Connection connection = ConnectionProvider.getConnection();
+        assertNotNull(connection);
+    }
+
+    @Test
+    public void testGetConnectionTwiceReturnsSameConnection() {
+        // Verify that getConnection returns the same connection object when called multiple times
+        Connection connection1 = ConnectionProvider.getConnection();
+        Connection connection2 = ConnectionProvider.getConnection();
+        assertSame(connection1, connection2);
+    }
+
+    @Test
+    public void testCloseConnection() {
+        // Verify that closeConnection closes the connection and sets it to null
+        Connection connection = ConnectionProvider.getConnection();
+        assertNotNull(connection);
+        ConnectionProvider.closeConnection();
+        try {
+            assertTrue(connection.isClosed());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            fail("Failed to close connection");
+        }
+        assertNull(ConnectionProvider.getConnection());
+    }
+}
---
+++ src/test/java/com/sparta/room3/EmployeeDAOTest.java
@@ -0,0 +1,86 @@
+package com.sparta.room3;
+
+import com.sparta.room3.model.EmployeeDAO;
+import com.sparta.room3.model.EmployeeDTO;
+import org.junit.jupiter.api.*;
+import java.sql.*;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class EmployeeDAOTest {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/?user=root"; // this needs a fix
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "password";
+    private static Connection connection;
+    private static List<EmployeeDTO> employeeList;
+    private static EmployeeDAO employeeDAO;
+
+    @BeforeAll
+    static void setup() throws SQLException {
+        connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+        employeeList = new ArrayList<>(Arrays.asList(
+                new EmployeeDTO(1, "Mr.", "John", null, "Doe", "M", "johndoe@example.com",
+                        Date.valueOf(LocalDate.of(1990, 1, 1)), Date.valueOf(LocalDate.now()), 50000.0),
+                new EmployeeDTO(2, "Ms.", "Jane", null, "Doe", "F", "janedoe@example.com",
+                        Date.valueOf(LocalDate.of(1995, 1, 1)), Date.valueOf(LocalDate.now()), 60000.0),
+                new EmployeeDTO(3, "Mrs.", "Samantha", null, "Smith", "F", "samanthasmith@example.com",
+                        Date.valueOf(LocalDate.of(1985, 1, 1)), Date.valueOf(LocalDate.now()), 70000.0)
+        ));
+        employeeDAO = new EmployeeDAO(connection, employeeList);
+    }
+
+    @AfterAll
+    static void teardown() throws SQLException {
+        connection.close();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Create Table Test")
+    void createTableTest() {
+        Assertions.assertDoesNotThrow(() -> employeeDAO.createTable());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Save All Employee List Test")
+    void saveAllEmployeeListTest() {
+        Assertions.assertDoesNotThrow(() -> employeeDAO.saveAllEmployeeList());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Find Employee By Id Test")
+    void findEmployeeByIdTest() throws SQLException {
+        EmployeeDTO employee = employeeDAO.findEmployeeById(1);
+        Assertions.assertNotNull(employee);
+        Assertions.assertEquals("Mr.", employee.getNamePrefix());
+        Assertions.assertEquals("John", employee.getFirstName());
+        Assertions.assertNull(employee.getMiddleInitial());
+        Assertions.assertEquals("Doe", employee.getLastName());
+        Assertions.assertEquals("M", employee.getGender());
+        Assertions.assertEquals("johndoe@example.com", employee.getEmail());
+        Assertions.assertEquals(Date.valueOf(LocalDate.of(1990, 1, 1)), employee.getDateOfBirth());
+        Assertions.assertEquals(Date.valueOf(LocalDate.now()), employee.getHireDate());
+        Assertions.assertEquals(50000.0, employee.getSalary());
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Update Employee Test")
+    void updateEmployeeTest() throws SQLException {
+        EmployeeDTO employee = employeeDAO.findEmployeeById(1);
+        employee.setNamePrefix("Dr.");
+        employee.setMiddleInitial("H");
+        employee.setSalary(60000.0);
+        Assertions.assertDoesNotThrow(() -> employeeDAO.updateEmployee(employee));
+
+        EmployeeDTO updatedEmployee = employeeDAO.findEmployeeById(1);
+        Assertions.assertNotNull(updatedEmployee);
+        Assertions.assertEquals("Dr.", updatedEmployee.getNamePrefix());
+        Assertions.assertEquals("H", updatedEmployee.getMiddleInitial());
+        Assertions.assertEquals(60000.0, updatedEmployee.getSalary());
+    }
+}
